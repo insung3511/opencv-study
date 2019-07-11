aeb65728540e7e27c2f05ba75b27b859290fe646
insung3511 <insung.park123@gmail.com>
2019-07-12 07:51:47 +0900

    open image & crop image

---
+++ openCV/opencv_openImage.py
@@ -0,0 +1,12 @@
+import imutils
+import cv2
+
+image = cv2.imread("./pictures/people.jpg")
+(h, w, d) = image.shape
+print("width={}, height={}, depth={}".format(w, h, d))
+
+(B, G, R) = image[100, 100]
+print("R={}, G={}, B={}".format(R,G,B))
+
+cv2.imshow("opencv", image)
+cv2.waitKey(0)
---
+++ openCV/opencv_resizingImage.py
@@ -0,0 +1,20 @@
+import imutils
+import cv2
+
+image = cv2.imread("./pictures/people.jpg")
+(h, w, d) = image.shape
+print("width={}, height={}, depth={}".format(w, h, d))
+
+(B, G, R) = image[100, 100]
+print("R={}, G={}, B={}".format(R,G,B))
+
+#resized = cv2.resize(image, (200, 200))
+#cv2.imshow("Fixed Resized", resized)
+r = 500.0 / w
+dim = (500, int(h * r))
+resized = cv2.resize(image, dim)
+cv2.imshow("Resized Picture", resized)
+cv2.waitKey(0)
+
+cv2.imshow("opencv", image)
+cv2.waitKey(0)
