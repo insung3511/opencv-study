dcd5de3a12309fa8b4c11123f6e1e43190c7dc0b
insung3511 <insung.park123@gmail.com>
2019-07-18 11:49:53 +0900

    공부 햇으닌까 자랑질
    고태건이 human detection 공부 하래요
    
    Co-Authored-By: 고태건 <gtg7784@naver.com>

--- openCV/Basic of Middle/opencv_objContuer.py
+++ openCV/Basic of Middle/opencv_objContuer.py
@@ -11,6 +11,8 @@ image = cv2.imread(args["image"])
 cv2.imshow("Image", image)
 cv2.waitKey(0)
 
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+thresh = cv2.threshold(gray, 225, 255, cv2.THRESH_BINARY_INV)[1]
 cnts = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL,
     cv2.CHAIN_APPROX_SIMPLE)
 cnts = imutils.grab_contours(cnts)
@@ -21,3 +23,9 @@ for c in cnts:
     cv2.imshow("Contours", output)
     cv2.waitKey(0)
     
+# draw the total number of contours found in purple
+text = "I found {} objects!".format(len(cnts))
+cv2.putText(output, text, (10, 25),  cv2.FONT_HERSHEY_SIMPLEX, 0.7,
+	(240, 0, 159), 2)
+cv2.imshow("Contours", output)
+cv2.waitKey(0)
