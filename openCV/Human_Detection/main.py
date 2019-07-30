32231ddac28fd8e96b9631f4002afe2d7e459d3d
insung3511 <insung3511>
2019-07-30 23:00:41 +0900

    opencv find face in walking_person.jpg

--- openCV/Human_Detection/main.py
+++ openCV/Human_Detection/main.py
@@ -1,10 +1,9 @@
-#!/Users/Bahk_InSung/opencv/bin/env python
 import cv2
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
 
-img = cv2.imread("./human_detection/standing_person.jpg")
+img = cv2.imread("./human_detection/walking_person.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 faces = face_cascade.detectMultiScale(gray, 1.3, 5)
