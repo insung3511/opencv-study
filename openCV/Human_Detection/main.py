74851399c86925ddbd1fb9f05ab84d3e8899f19b
insung3511 <insung3511>
2019-07-31 10:08:20 +0900

    Some Problem...

--- openCV/Human_Detection/main.py
+++ openCV/Human_Detection/main.py
@@ -3,10 +3,10 @@ import cv2
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 eye_cascade = cv2.CascadeClassifier("haarcascade_eye.xml")
 
-img = cv2.imread("./human_detection/walking_person.jpg")
+img = cv2.imread("../pictures/people.jpg")
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+faces = face_cascade.detectMultiScale(gray, 1.5,5)
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
     roi_gray = gray[y:y+h, x:x+w]
