e8172a640595570b7009e73a4fb2adf434b93abd
insung3511 <insung.park123@gmail.com>
2019-08-02 00:59:27 +0900

    Subway.mp4 에서 뒷모습 찾기 성공
    흐아 죽어 가는중 입니다..

--- openCV/Human_Detection/human.py
+++ openCV/Human_Detection/human.py
@@ -1,42 +1,27 @@
-import cv2 as cv
-import numpy as np 
-from matplotlib import pyplot as plt
+# import the necessary packages
+import numpy as np
+import cv2
+ 
+# initialize the HOG descriptor/person detector
+hog = cv2.HOGDescriptor()
+hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
+cv2.startWindowThread()
+# open webcam video stream
+cap = cv2.VideoCapture('subway.mp4')
+while(True):
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, (640, 480))
+    gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
+    boxes, weights = hog.detectMultiScale(frame, winStride=(8,8) )
+    boxes = np.array([[x, y, x + w, y + h] for (x, y, w, h) in boxes])
+    for (xA, yA, xB, yB) in boxes:
+        cv2.rectangle(frame, (xA, yA), (xB, yB),
+                          (0, 255, 0), 2)
+    
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
 
-path = "./human_detection/walking_person.jpg"
-
-think_1 = cv.imread('./' + path)
-grayThink_1 = cv.cvtColor(think_1, cv.COLOR_BGR2HSV)
-
-think_2 = cv.imread('./' + path)
-grayThink_2 = cv.cvtColor(think_2, cv.COLOR_BGR2HSV)
-
-think_3 = cv.imread('./' + path)
-grayThink_3 = cv.cvtColor(think_3, cv.COLOR_BGR2HSV)
-
-body_cascade = cv.CascadeClassifier('haarcascade_fullbody.xml')
-
-#Think_1
-body = body_cascade.detectMultiScale(grayThink_1, 1.01, 10, minSize=(30, 30))
-for (x, y, w, h) in body:
-    cv.rectangle(think_1, (x, y), (x+w, y+h), (0, 0, 255), 3)
-
-plt.subplot(1, 3, 1)
-plt.imshow(think_1, cmap='gray')
-
-#Think_2
-body = body_cascade.detectMultiScale(grayThink_2,1.01, 2, 0, minSize=(70, 70))
-for (x,y,w,h) in body :        
-    cv.rectangle(think_2,(x,y),(x+w,y+h),(0,0,255),3)
-
-plt.subplot(1, 3, 2)
-plt.imshow(think_2, cmap='gray')
-
-
-body = body_cascade.detectMultiScale(grayThink_3,1.01, 2, 0, minSize=(70, 70))
-for (x,y,w,h) in body :        
-    cv.rectangle(think_2,(x,y),(x+w,y+h),(0,0,255),3)
-
-plt.subplot(1, 3, 3)
-plt.imshow(think_2, cmap='gray')
-
-plt.show()
+cap.release()
+cv2.destroyAllWindows()
+cv2.waitKey(1)
