9ca89173b82eee2c0294a65225df8ad7958fc9dd
insung3511 <insung.park123@gmail.com>
2019-08-03 01:32:25 +0900

    Update face.py

--- openCV/Human_Detection/face.py
+++ openCV/Human_Detection/face.py
@@ -10,6 +10,7 @@ faces = face_cascade.detectMultiScale(gray, 5.5,5)
 for (x, y, w, h) in faces:
     cv2.rectangle(img, (x,y), (x+w, y+h), (255, 0, 0), 2)
     roi_gray = gray[y:y+h, x:x+w]
+    roi_color = img[y:y+h, x:x+w]
     eyes = eye_cascade.detectMultiScale(roi_gray)
     for (ex, ey, ew, eh) in eyes:
         cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
