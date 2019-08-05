17f9a8362adece8e0db15fe71ea4b71a35414bb5
insung3511 <insung.park123@gmail.com>
2019-08-06 00:12:35 +0900

    Update

--- openCV/Basic of Middle/opencv_threasholding.py
+++ openCV/Basic of Middle/opencv_threasholding.py
@@ -14,4 +14,4 @@ cv2.waitKey(0)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 thresh = cv2.threshold(gray, 225, 255, cv2.THRESH_BINARY_INV)[1]
 cv2.imshow("Thresh", thresh)
-cv2.waitKey(0)
+cv2.waitKey(0)  
